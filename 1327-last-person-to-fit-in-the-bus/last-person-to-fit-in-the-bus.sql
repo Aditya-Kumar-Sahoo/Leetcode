17e7e0aff27d36ef24abb6f84a9e1aea5ce97d7b
Aditya-Kumar-Sahoo <132480323+Aditya-Kumar-Sahoo@users.noreply.github.com>
2026-04-24 22:43:39 +0530

    Time: 764 ms (67.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT person_name
+FROM
+(SELECT
+    person_name,
+    SUM(weight) OVER(ORDER BY turn ROWS BETWEEN UNBOUNDEd PRECEDING AND CURRENT ROW) AS total
+FROM Queue)t
+WHERE total <= 1000
+ORDER BY total DESC
+LIMIT 1;
